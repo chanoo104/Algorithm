1f217c54bf853f9bed1cb0aa948e10b1dc43516f
chanoo104 <chanoo104@naver.com>
2021-05-03 20:37:09 +0900

    1011 문제

--- Main.java
+++ Main.java
@@ -3,26 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] apt = new int[15][15];
-        for (int i = 0; i < 15; i++) {
-            apt[i][1] = 1;
-            apt[0][i] = i;
-        }
-        int sum = 0;
-        for (int i = 1; i < 15; i++) {
-            for (int j = 1; j <= 15; j++) {
-                for (int k = 0; k < j; k++) {
-                    sum += apt[i - 1][k];
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int distance = y - x;
+            int count;
+            int max = (int) Math.sqrt(y - x);
+            if (max == Math.sqrt(distance)) {
+                count = max * 2 - 1;
+            } else {
+                if (distance <= max * max + max) {
+                    count = max * 2;
+                } else {
+                    count = max * 2 + 1;
                 }
-                apt[i][j-1] = sum;
-                sum = 0;
             }
-        }
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int k = sc.nextInt();
-            int n = sc.nextInt();
-            System.out.println(apt[k][n]);
+            System.out.println(count);
         }
     }
 }
